2979ec5d60d650c2793923da2b5ebbd236562424
Leesoo Ahn <dev@ooseel.net>
2020-08-18 22:40:50 +0900

    add insmod.sh script
    The drv must be insmod-ed through high to low priority of dependencies.
    This script takes the responsibility for doing so in a row.

---
+++ insmod.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+mod_list=("rt2x00lib.ko" "rt2x00usb.ko" "rt2800lib.ko" "rt2800usb.ko")
+
+PREFIX="./rt2x00"
+
+# check all needs are existed
+for item in "${mod_list[@]}"; do
+  echo "> checking ${item} ..."
+  [ -e ${PREFIX}/${item} ] || { exit 1; }
+done
+echo "* all needs are existed!"
+echo ""
+
+for item in "${mod_list[@]}"; do
+  echo "> insmod-ing ${item} ..."
+  sudo insmod ${PREFIX}/${item} || { echo "* failed on ${item}"; exit 2; }
+done
+echo "* job complete!"
